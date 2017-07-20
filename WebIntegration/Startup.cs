b03219b69e059b9dcfa85a7959cab2ce423a3f65
shahedc <shahedc@gmail.com>
2017-07-20 02:35:45 -0400

    added web integration tests
    added web integration tests with WebIntegration project

---
+++ WebIntegration/Startup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using WebIntegration.Services;
+
+namespace WebIntegration
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                if (context.Request.Path.Value.Contains("checkprime"))
+                {
+                    int numberToCheck;
+                    try
+                    {
+                        numberToCheck = int.Parse(context.Request.QueryString.Value.Replace("?", ""));
+                        var primeService = new PrimeService();
+                        if (primeService.IsPrime(numberToCheck))
+                        {
+                            await context.Response.WriteAsync($"{numberToCheck} is prime!");
+                        }
+                        else
+                        {
+                            await context.Response.WriteAsync($"{numberToCheck} is NOT prime!");
+                        }
+                    }
+                    catch
+                    {
+                        await context.Response.WriteAsync("Pass in a number to check in the form /checkprime?5");
+                    }
+                }
+                else
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                }
+            });
+        }
+    }
+}
---
+++ WebIntegration/Tests/PrimeTests.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WebIntegration.Tests
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class PrimeTests
+    {
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+        
+        public PrimeTests()
+        {
+            // Arrange
+            _server = new TestServer(new WebHostBuilder()
+                .UseStartup<Startup>());
+            _client = _server.CreateClient();
+        }
+
+
+        [Fact]
+        public async Task ReturnHelloWorld()
+        {
+            // Act
+            var response = await _client.GetAsync("/");
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.Equal("Hello World!",
+                responseString);
+        }
+
+        private async Task<string> GetCheckPrimeResponseString(
+       string querystring = "")
+        {
+            var request = "/checkprime";
+            if (!string.IsNullOrEmpty(querystring))
+            {
+                request += "?" + querystring;
+            }
+            var response = await _client.GetAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        [Fact]
+        public async Task ReturnInstructionsGivenEmptyQueryString()
+        {
+            // Act
+            var responseString = await GetCheckPrimeResponseString();
+
+            // Assert
+            Assert.Equal("Pass in a number to check in the form /checkprime?5",
+                responseString);
+        }
+        [Fact]
+        public async Task ReturnPrimeGiven5()
+        {
+            // Act
+            var responseString = await GetCheckPrimeResponseString("5");
+
+            // Assert
+            Assert.Equal("5 is prime!",
+                responseString);
+        }
+
+        [Fact]
+        public async Task ReturnNotPrimeGiven6()
+        {
+            // Act
+            var responseString = await GetCheckPrimeResponseString("6");
+
+            // Assert
+            Assert.Equal("6 is NOT prime!",
+                responseString);
+        }
+    }
+}
